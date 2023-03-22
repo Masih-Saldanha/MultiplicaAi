bc1a287e350e63ae6dd5ab81cf48bd1f4ac4144e
Masih <masih.saldanha@gmail.com>
2023-03-22 18:58:26 -0300

    feat: add Scanner to get int input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número: ");
+        final int number = sc.nextInt();
+
+        System.out.println("Tabela de multiplicação de " + number);
+        // for (int i = 0; i < args.length; i++) {
+            
+        // }
     }
 }
